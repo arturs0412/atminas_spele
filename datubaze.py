2944f132c2745adb65dda15660c522368e0f18c4
arturs0412 <116163004+arturs0412@users.noreply.github.com>
2025-02-05 12:38:23 +0200

    dddnbciehbfghjz

--- datubaze.py
+++ datubaze.py
@@ -1,23 +1,23 @@
 import sqlite3
 
-def savienot():
-  DB = sqlite3.connect('dati.db')
-  return DB.cursor()
+def get_top_results():
+  conn = sqlite3.connect('datubaze.db')
+  c = con.cursor()
+  c.execute("SELECT * FROM rezultati")
+  rezultati = c.fetchall()
+  conn.close()
+  dati = [
+    {"id": r[0], "vards": r[1], "results": r[2], "laiks": r[3], "datums": r[4]}
+    for r in resultati
+  ]
+  return dati
 
-def top():
-  SQL = savienot()
-  SQL.execute("SELECT * FROM rezultati")
-  rezultati = SQL.fetchall()
-  dati = []
-  for rez in rezultati:
-    dati.append({
-      "id":rez[0],
-      "vards":rez[1],
-      "klikski":rez[2],
-      "laiks":rez[3],
-      "datums":rez[4]
-    })
   return dati
 
 def pievienot(dati):
-  ffffgvgfgggg
+  conn = sqlite3.connect('datubaze.db')
+  c = con.cursor()
+  c.execute("""
+    INSERT INTO rezultati (vards, rezultats, laiks, datums) VALUES (?, ?, ?, ?)""", (dati["vards"], dati["rezultats"], dati["laiks"], dati["datums"]))
+  conn.commit()
+  conn.close()
