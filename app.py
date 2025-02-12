e34d39c15e2607c5300a783c5d91eae9f8af627e
arturs0412 <116163004+arturs0412@users.noreply.github.com>
2025-02-12 12:55:27 +0200

    adesfgevtbf

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import json
+from datubaze import get_top_results, pievienot
 app = Flask(__name__)
 
 #app = Flask('app')
@@ -21,6 +22,28 @@ def top():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/topData', methods=['GET'])
+def top_data():
+    try:
+        top_resultati = get_top_results()
+        top_5 = sorted(top_resultati, key=lambda x: (x['klikski']))[:5]
+        return jsonify(top_5), 200
+    except Exception:
+        return jsonify({'status': 'error'}), 500
+    
+@app.route('/pievienot-rezultatu', methods=['POST'])
+def pievienot_rezultatu():
+    dati = requrst.json
+    try:
+        pievienot(dati)
+        top_5 = sorted(get_top_results(), key=lambda x: (x['klikski']))[:5]
+        with open('result.json', 'w', encoding='utf-8') as fail:
+            json.dump(top_5, fail, ensure_ascii=False, indent=4)
+            return jsonify({'status': 'sucess'}), 200
+    except Exception:
+        return jsonify({'status': 'error'}), 500
+
 
 if __name__ == '__main__':
   #app.run(host='0.0.0.0', port=80)
